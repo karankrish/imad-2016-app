91cbdf4adad50c7fa6f5a92c69f4bb11d7b28442
karankrish <mail2balanair@gmail.com>
2016-09-27 19:20:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,43 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne={
+    title:'Article One |karan krishna',
+    heading:'Article one',
+    date:'sep 5 2016',
+    content:'                <p>This is content for my first article</p>'
+};
+
+var htmlTemplate=` 
+<html>
+    <head>
+        <title>
+            ${title}
+        </title>
+        <meta name="viewport" content="width=device-width , initial-scale=1"/>
+         <link href="/ui/style.css" rel="stylesheet" />
+  
+    </head>
+     <body>
+    <div class="contaner">     
+        <div>
+        <a href ="/">Home</a>
+        </div>
+        <hr/>
+        <h3> ${heading}</h3>
+        <div>
+            ${date}
+            </div>
+            <div>
+                ${content}
+                </div>
+    </div>
+    </body>
+</html> `
+
+
+
+
 
 app.get('/article-one',function (req,res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
