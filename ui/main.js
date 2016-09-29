d0921bc4c3bb6c9c6824bca05dcc78f8c0ed5a58
karankrish <mail2balanair@gmail.com>
2016-09-29 11:42:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,11 @@ console.log('Loaded!');
 
 var element=document.getElementById('main-text');
 element.innerHTML='New Value';
+
+
+var element =document.getElementById('img');
+img.onclick = function() {
+    
+    img.style.marginLeft ='100px';
+    
+};
