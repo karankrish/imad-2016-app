b179ede3c6ae218907005526af29585496c402d3
karankrish <mail2balanair@gmail.com>
2016-09-29 17:30:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ var htmlTemplate=`
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-var counter=0;
+var counter= 0;
 app.get('/counter', function(req, res) {
    counter = counter+1;
    res.send(counter.toString());
