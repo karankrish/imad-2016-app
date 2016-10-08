a16f7e93692fdf4e878e6aae775437a52db782fd
karankrish <mail2balanair@gmail.com>
2016-10-08 17:16:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,17 +5,30 @@ button.onclick = function() {
     var span=document.getElementById('count');
     span.innerHTML=count.toString();
 };
-var nameInput=document.getElementById('name');
-var name=nameInput.values;
+
 var submit=document.getElementById('submit_btn');
 submit.onclick=function()
 {
-    var names=['name1','name2','name3'];
+var request=new XMLHttpRequest();
+request.onreadystatechange=function()
+{
+    if(request.readyState===XMLHttpRequest.DONE){
+        if(request.status===200){
+           var names=['name1','name2','name3'];
     var list='';
     for(var i=0;i<names.length;i++)
     {
         lit+='<li>'+names[i]+'</li>';
     }
     var ul=document.getElementById('namelist');
-    ul.ineerHTML=list;
+    ul.ineerHTML=list;  
+        }
+    }
+
+   };
+   var nameInput=document.getElementById('name');
+var name=nameInput.values;
+request.open('GET','http://http://karankrish.imad.hasura-app.io/submit-name?name='+name,true);
+request.send(null);
 };
+   
