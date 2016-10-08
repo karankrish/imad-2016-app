9b734b837a2614f5b7e7f7d768f57ed0b9eec3e1
karankrish <mail2balanair@gmail.com>
2016-10-08 17:28:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,7 +27,7 @@ request.onreadystatechange=function()
 
    };
    var nameInput=document.getElementById('name');
-var name=nameInput.values;
+var name=nameInput.value;
 request.open('GET','http://http://karankrish.imad.hasura-app.io/submit-name?name='+name,true);
 request.send(null);
 };
