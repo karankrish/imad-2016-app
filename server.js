55b8664f448cc0318ed384fbe8aac1d062173431
karankrish <mail2balanair@gmail.com>
2016-10-28 19:46:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -70,8 +70,15 @@ var htmlTemplate=`
     return htmlTemplate; 
 }
 
+var pool = new Pool(config);
 app.get('/test-db',function(req, res){
-    
+    pool.query('SELECT * FROM test', function (err, result){
+        if(err){
+            res.status(500).send(err.toString());
+        }else {
+            res.send(JSON.stringify(result));
+        }
+    });
 });
 
 
