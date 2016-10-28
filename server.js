30c1d1e9607cf216832011f7896e3d104da7dea6
karankrish <mail2balanair@gmail.com>
2016-10-28 22:19:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -110,8 +110,8 @@ app.get('/articles/:articleName',function (req,res) {
         res.status(404).send('Article not found');
         }
         else {
-            var articleData=result.rows[0];
-            res.send(createTemplate(articleData));
+            var articlesData=result.rows[0];
+            res.send(createTemplate(articlesData));
         
         }
         }
