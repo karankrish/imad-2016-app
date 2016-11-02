8fc18b881eded6d675438b036da9696ce74ef0b7
karankrish <mail2balanair@gmail.com>
2016-11-02 14:16:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,34 +1,16 @@
-var button=document.getElementById('counter');
-var count=0;
-button.onclick = function() {
-    count=count+1;
-    var span=document.getElementById('count');
-    span.innerHTML=count.toString();
-};
+alert("USE internet explorer ");
+$(document).ready(function(){
+  setBindings();
+})
+function setBindings(){
+  $("nav a").click(function(e){
+    e.preventDefault();
+    var sectionID=e.currentTarget.id+"Section";
+    $('html,body').animate({
+      scrollTop:$("#"+sectionID).offset().top
+    },'10000')
+  })
 
-var submit=document.getElementById('submit_btn');
-submit.onclick=function()
-{
-var request=new XMLHttpRequest();
-request.onreadystatechange=function()
-{
-    if(request.readyState===XMLHttpRequest.DONE){
-        if(request.status===200){
-           var names=['name1','name2','name3'];
-    var list='';
-    for(var i=0;i<names.length;i++)
-    {
-        lit+='<li>'+names[i]+'</li>';
-    }
-    var ul=document.getElementById('namelist');
-    ul.ineerHTML=list;  
-        }
-    }
 
-   };
-   var nameInput=document.getElementById('name');
-var name=nameInput.value;
-request.open('GET','http://http://karankrish.imad.hasura-app.io/submit-name?name='+name,true);
-request.send(null);
-};
-   
+
+}
