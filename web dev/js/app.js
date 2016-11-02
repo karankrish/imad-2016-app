602a4defb4789c07fcd42c675df3aabab008348f
karankrish <mail2balanair@gmail.com>
2016-11-02 14:23:11 +0530

    Add files via upload

---
+++ web dev/js/app.js
@@ -0,0 +1,16 @@
+alert("USE internet explorer ");
+$(document).ready(function(){
+  setBindings();
+})
+function setBindings(){
+  $("nav a").click(function(e){
+    e.preventDefault();
+    var sectionID=e.currentTarget.id+"Section";
+    $('html,body').animate({
+      scrollTop:$("#"+sectionID).offset().top
+    },'10000')
+  })
+
+
+
+}
